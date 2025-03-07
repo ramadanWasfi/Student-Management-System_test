f2b0703fa663e3af4a975090eb6bc901ba9db304
Ramadan Wasfi <ramadanwasfi2@gmail.com>
2025-03-08 00:34:12 +0200

    Add CRUD

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -3,26 +3,58 @@ const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updat
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
     //write your code
+    try {
+        let students = getAllStudents(req);
+        console.log(students);
+        res.send(students);
+    } catch(error) {
+        throw error;
+    }
 
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
     //write your code
-
+    try {
+        addNewStudent(req);
+        res.send(200);
+    }catch(error){
+        throw error;
+    }
+   
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
     //write your code
+    try {
+        let result = updateStudent(req.params.id);
+        res.send(result);
+    } catch(error) {
+        throw error;
+    }
+    
 
 });
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
     //write your code
+    try {
+        let result = getStudentDetail(req.params.id);
+        res.send(result);
+    } catch(error) {
+        throw error;
+    }
 
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
     //write your code
+    try {
+        let result = setStudentStatus(req.params);
+        res.send(result);
+    } catch(error) {
+        throw error;
+    }
 
 });
 
